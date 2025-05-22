e71e25d18629b3875d4d17b9c86f3f1f960cbeb3
Jigarunagar <officialjigarunagar@gmail.com>
2025-05-22 11:56:58 +0530

    update

--- script/signup.js
+++ script/signup.js
@@ -1,13 +1,27 @@
 document.querySelector("#signupform").addEventListener("submit", async (e) => {
   e.preventDefault();
+
+  const fullname = document.getElementById("DataName").value;
+  const email = document.getElementById("DataEmail").value;
+  const number = document.getElementById("DataNumber").value;
+  const password = document.getElementById("DataPassword").value;
+
+  const nameRegex = /^[A-Za-z\s]+$/;
+
+  if (!nameRegex.test(fullname)) {
+    alert("only letters and spaces.");
+    return;
+  } 
   const user = {
-    fullname: document.getElementById("DataName").value,
-    email: document.getElementById("DataEmail").value,
-    number: document.getElementById("DataNumber").value,
-    password: document.getElementById("DataPassword").value,
+    fullname,
+    email,
+    number,
+    password,
   };
 
- const res = await fetch("http://localhost:5000/signup", {
+  console.log(user);
+
+  const res = await fetch("http://localhost:5000/signup", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(user),
@@ -15,4 +29,8 @@ document.querySelector("#signupform").addEventListener("submit", async (e) => {
 
   const result = await res.json();
   alert(result.message);
+
+  if (result.message == "Signup successful!") {
+    window.location.href = "index.html";
+  }
 });
